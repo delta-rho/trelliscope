b19f431571be3b8f5adf100075a2f754c0fa3f47
Ryan Hafen <rhafen@gmail.com>
2014-03-24 07:56:32 -0700

    Add local data syncing for helping sync files to a web server

--- R/websync.R
+++ R/websync.R
@@ -11,14 +11,14 @@
 #' 
 #' @author Ryan Hafen
 #' 
-#' @seealso \code{\link{typeset}}, \code{\link{webConn}}
+#' @seealso \code{\link{typeset}}, \code{\link{webConn}}, \code{\link{syncLocalData}}
 #' 
 #' @export
-websync <- function(
-   vdbConn=getOption("vdbConn"),
-   webConn=getOption("vdbWebConn"),
-   verbose=FALSE,
-   rsync=NULL
+webSync <- function(
+   vdbConn = getOption("vdbConn"),
+   webConn = getOption("vdbWebConn"),
+   verbose = FALSE,
+   rsync = NULL
 ) {
    
    # conn <- getOption("vdbConn")
@@ -30,44 +30,86 @@ websync <- function(
    
 	# TODO: check to make sure passwordless ssh is set up
    
+   syncLocalData(vdbConn)
+   
    sshFlag <- "-e ssh "
    user <- ""
    if(!is.null(webConn$user))
-      user <- paste(webConn$user, "@", sep="")
+      user <- paste(webConn$user, "@", sep = "")
    
-   message("Syncing local vdb directory to web server...")
+   message("* Syncing local vdb directory to web server...")
    system(paste(rsync, " -a -v ", sshFlag, 
       path.expand(vdbConn$path), "/* ",
       user, webConn$ip, ":", webConn$appDir, "/", webConn$name,
-      sep=""
-   ), intern=verbose, ignore.stderr=!verbose, ignore.stdout=!verbose)
+      sep = ""
+   ), intern = verbose, ignore.stderr =! verbose, ignore.stdout =! verbose)
    
    ## make a copy from viewerDir to appDir/vdbName
-   pkgPath <- system.file(package="trelliscope")
+   pkgPath <- system.file(package = "trelliscope")
    
-   message("Syncing latest shiny viewer to web app directory...")
+   message("* Syncing latest shiny viewer to web app directory...")
    # system("ssh host \"mkdir -p /home/hafe647/shiny\"")
    
    system(paste(rsync, " -a -v ", sshFlag, 
       file.path(pkgPath, "trelliscopeViewer"), " ",
       user, webConn$ip, ":", webConn$appDir, "/", webConn$name,
-      sep=""
-   ), intern=verbose, ignore.stderr=!verbose, ignore.stdout=!verbose)
+      sep = ""
+   ), intern = verbose, ignore.stderr =! verbose, ignore.stdout =! verbose)
    
    NULL
 }
 
-findRsync <- function(rsync=NULL, verbose = "FALSE") {
+#' Sync localDisk Objects to VDB
+#' 
+#' Sync localDisk data that is used for VDB displays located throughout the system to a 'data' directory inside the VDB - useful for collecting data before syncing with a web server, and used inside of \code{\link{webSync}}.
+#' 
+#' @param vdbConn VDB connection settings
+#' @param rsync location of rsync binary
+#' 
+#' @author Ryan Hafen
+#' 
+#' @seealso \code{\link{webSync}}, \code{\link{webConn}}
+#' 
+#' @export
+syncLocalData <- function(vdbConn = getOption("vdbConn"), rsync = NULL) {
+   if(is.null(rsync))
+      rsync <- findRsync()
+   
+   # if there are localDisk objects, put them in VDB
+   load(file.path(vdbConn$path, "displays", "_displayList.Rdata"))
+   ldDisp <- subset(displayListDF, dataClass == "kvLocalDisk")
+   if(nrow(ldDisp) > 0) {
+      message("* Syncing localDisk objects to vdb 'data' directory...")
+      
+      newDatDir <- file.path(vdbConn$path, "data")
+      if(!file.exists(newDatDir))
+         dir.create(newDatDir, recursive = TRUE)
+      
+      locs <- sapply(seq_len(nrow(ldDisp)), function(i) {
+         tmp <- getDisplay(ldDisp$name[i], ldDisp$group[i])
+         getAttribute(tmp$panelDataSource, "conn")$loc
+      })
+      locs <- unique(locs)
+      if(any(duplicated(basename(locs))))
+         stop("Currently can't sync multiple localDisk data sets with same name but different paths.")
+      
+      for(ll in locs) {
+         system(paste(rsync, "-avh", ll, newDatDir))
+      }
+   }
+}
+
+findRsync <- function(rsync = NULL, verbose = "FALSE") {
    
    # http://www.rsync.net/resources/howto/windows_rsync.html
    
-   errorMsg <- "rsync executable not found. Use rsync= argument to specify the correct path."
+   errorMsg <- "rsync executable not found. Use 'rsync' argument to specify the correct path."
    
    if (is.null(rsync))
       rsync = "rsync"
    
    rsync = Sys.which(rsync)
-   if (rsync=="" || rsync=="rsync")
+   if (rsync == "" || rsync == "rsync")
       stop(errorMsg)
    
    if (.Platform$OS == "windows") {
