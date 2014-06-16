e1befd2cb94c29a6dfbe975fb9959c952c871c8c
Ryan Hafen <rhafen@gmail.com>
2014-06-16 12:51:35 -0400

    Update to work with shiny 0.10.0

--- R/view.R
+++ R/view.R
@@ -58,16 +58,19 @@ view <- function(name=NULL, group=NULL, openBrowser=TRUE, conn=getOption("vdbCon
 }
 
 ## internal (from shiny's runApp() - need to be able to add hash string)
-myRunApp <- function(appDir = getwd(), port = NULL, launch.browser = getOption("shiny.launch.browser", interactive()), host = getOption("shiny.host", "127.0.0.1"), workerId = "", quiet = FALSE, display.mode = c("auto", "normal", "showcase"), hash="") {
+myRunApp <- function (appDir = getwd(), port = NULL, launch.browser = getOption("shiny.launch.browser", interactive()), host = getOption("shiny.host", "127.0.0.1"), workerId = "", quiet = FALSE, display.mode = c("auto", "normal", "showcase"), hash="") {
+   on.exit({
+      shiny:::handlerManager$clear()
+   }, add = TRUE)
    if (is.null(host) || is.na(host)) 
       host <- "0.0.0.0"
    ops <- options(warn = 1)
-   on.exit(options(ops))
+   on.exit(options(ops), add = TRUE)
+   shiny:::workerId(workerId)
    if (nzchar(Sys.getenv("SHINY_PORT"))) {
       ver <- Sys.getenv("SHINY_SERVER_VERSION")
       if (compareVersion(ver, .shinyServerMinVersion) < 0) {
-         warning("Shiny Server v", .shinyServerMinVersion, 
-         " or later is required; please upgrade!")
+         warning("Shiny Server v", .shinyServerMinVersion, " or later is required; please upgrade!")
       }
    }
    shiny:::setShowcaseDefault(0)
@@ -88,11 +91,8 @@ myRunApp <- function(appDir = getwd(), port = NULL, launch.browser = getOption("
       shiny:::setShowcaseDefault(0)
    else if (display.mode == "showcase") 
       shiny:::setShowcaseDefault(1)
-   
    require(shiny)
-   
-   .globals <- get(".globals", loadNamespace("shiny"))
-   
+   .globals <- get(".globals", loadNamespace("shiny")) ##
    if (is.null(port)) {
       for (i in 1:20) {
          if (!is.null(.globals$lastPort)) {
@@ -100,38 +100,36 @@ myRunApp <- function(appDir = getwd(), port = NULL, launch.browser = getOption("
             .globals$lastPort <- NULL
          }
          else {
-            port <- round(runif(1, min = 3000, max = 8000))
+            port <- shiny:::p_randomInt(3000, 8000)
          }
          tmp <- try(httpuv:::startServer(host, port, list()), silent = TRUE)
-         if (!is(tmp, "try-error")) {
+         if (!inherits(tmp, "try-error")) {
             httpuv:::stopServer(tmp)
             .globals$lastPort <- port
             break
          }
       }
    }
-   if (is.character(appDir)) {
-      orig.wd <- getwd()
-      setwd(appDir)
-      on.exit(setwd(orig.wd), add = TRUE)
-      server <- shiny:::startAppDir(port = port, host = host, workerId = workerId, quiet = quiet)
-   } else {
-      server <- shiny:::startAppObj(appDir$ui, appDir$server, port = port, host = host, workerId = workerId, quiet = quiet)
-   }
+   appParts <- as.shiny.appobj(appDir)
+   if (!is.null(appParts$onStart)) 
+      appParts$onStart()
+   if (!is.null(appParts$onEnd)) 
+      on.exit(appParts$onEnd(), add = TRUE)
+   server <- shiny:::startApp(appParts, port, host, quiet)
    on.exit({
       httpuv:::stopServer(server)
    }, add = TRUE)
    if (!is.character(port)) {
-      browseHost <- if (identical(host, "0.0.0.0"))
+      browseHost <- if (identical(host, "0.0.0.0")) 
          "127.0.0.1"
       else host
-      
       appUrl <- paste("http://", browseHost, ":", port, hash, sep = "")
       if (is.function(launch.browser)) 
-      launch.browser(appUrl)
+         launch.browser(appUrl)
       else if (launch.browser) 
-      utils::browseURL(appUrl)
-   } else {
+         utils::browseURL(appUrl)
+   }
+   else {
       appUrl <- NULL
    }
    shiny:::callAppHook("onAppStart", appUrl)
@@ -146,6 +144,97 @@ myRunApp <- function(appDir = getwd(), port = NULL, launch.browser = getOption("
    })
    return(.globals$retval)
 }
+
+
+## v0.9.1
+# myRunApp <- function(appDir = getwd(), port = NULL, launch.browser = getOption("shiny.launch.browser", interactive()), host = getOption("shiny.host", "127.0.0.1"), workerId = "", quiet = FALSE, display.mode = c("auto", "normal", "showcase"), hash="") {
+#    if (is.null(host) || is.na(host)) 
+#       host <- "0.0.0.0"
+#    ops <- options(warn = 1)
+#    on.exit(options(ops))
+#    if (nzchar(Sys.getenv("SHINY_PORT"))) {
+#       ver <- Sys.getenv("SHINY_SERVER_VERSION")
+#       if (compareVersion(ver, .shinyServerMinVersion) < 0) {
+#          warning("Shiny Server v", .shinyServerMinVersion, 
+#          " or later is required; please upgrade!")
+#       }
+#    }
+#    shiny:::setShowcaseDefault(0)
+#    if (is.character(appDir)) {
+#       desc <- shiny:::file.path.ci(appDir, "DESCRIPTION")
+#       if (file.exists(desc)) {
+#          settings <- read.dcf(desc)
+#          if ("DisplayMode" %in% colnames(settings)) {
+#             mode <- settings[1, "DisplayMode"]
+#             if (mode == "Showcase") {
+#                shiny:::setShowcaseDefault(1)
+#             }
+#          }
+#       }
+#    }
+#    display.mode <- match.arg(display.mode)
+#    if (display.mode == "normal") 
+#       shiny:::setShowcaseDefault(0)
+#    else if (display.mode == "showcase") 
+#       shiny:::setShowcaseDefault(1)
+#    
+#    require(shiny)
+#    
+#    .globals <- get(".globals", loadNamespace("shiny"))
+#    
+#    if (is.null(port)) {
+#       for (i in 1:20) {
+#          if (!is.null(.globals$lastPort)) {
+#             port <- .globals$lastPort
+#             .globals$lastPort <- NULL
+#          }
+#          else {
+#             port <- round(runif(1, min = 3000, max = 8000))
+#          }
+#          tmp <- try(httpuv:::startServer(host, port, list()), silent = TRUE)
+#          if (!is(tmp, "try-error")) {
+#             httpuv:::stopServer(tmp)
+#             .globals$lastPort <- port
+#             break
+#          }
+#       }
+#    }
+#    if (is.character(appDir)) {
+#       orig.wd <- getwd()
+#       setwd(appDir)
+#       on.exit(setwd(orig.wd), add = TRUE)
+#       server <- shiny:::startAppDir(port = port, host = host, workerId = workerId, quiet = quiet)
+#    } else {
+#       server <- shiny:::startAppObj(appDir$ui, appDir$server, port = port, host = host, workerId = workerId, quiet = quiet)
+#    }
+#    on.exit({
+#       httpuv:::stopServer(server)
+#    }, add = TRUE)
+#    if (!is.character(port)) {
+#       browseHost <- if (identical(host, "0.0.0.0"))
+#          "127.0.0.1"
+#       else host
+#       
+#       appUrl <- paste("http://", browseHost, ":", port, hash, sep = "")
+#       if (is.function(launch.browser)) 
+#       launch.browser(appUrl)
+#       else if (launch.browser) 
+#       utils::browseURL(appUrl)
+#    } else {
+#       appUrl <- NULL
+#    }
+#    shiny:::callAppHook("onAppStart", appUrl)
+#    on.exit({
+#       shiny:::callAppHook("onAppStop", appUrl)
+#    }, add = TRUE)
+#    .globals$retval <- NULL
+#    .globals$stopped <- FALSE
+#    shiny:::shinyCallingHandlers(while (!.globals$stopped) {
+#       shiny:::serviceApp()
+#       Sys.sleep(0.001)
+#    })
+#    return(.globals$retval)
+# }
 
 
 
