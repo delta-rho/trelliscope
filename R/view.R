7e1afc858d38e1a304eae1a140e4df8138bcbf28
Barret Schloerke <schloerke@gmail.com>
2014-06-19 13:57:19 -0400

    added .shinyServerMinVersion to globalVariables

--- R/view.R
+++ R/view.R
@@ -1,3 +1,8 @@
+if(getRversion() >= "2.15.1") {
+  utils::globalVariables(c(".shinyServerMinVersion"))
+}
+
+
 #' View a Display or Run Shiny Display Viewer
 #'
 #' View a display or run Shiny display viewer
