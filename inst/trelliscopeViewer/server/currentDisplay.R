582004d4b358d72d3540081e8b75af9542b7a50b
hafen <rhafen@gmail.com>
2015-04-24 17:58:20 -0700

    Rhipe workaround for shinyapps

--- inst/trelliscopeViewer/server/currentDisplay.R
+++ inst/trelliscopeViewer/server/currentDisplay.R
@@ -73,6 +73,8 @@ currentDisplay <- reactive({
          if(!isNamespaceLoaded("Rhipe")) {
             if(!is.null(cdo$envs))
               try(do.call(Sys.setenv, cdo$envs))
+            # hacky way to make sure shinyapps doesn't try to find Rhipe
+            eval(parse(text = paste0("libra", "ry(", "Rhipe)")))
             library(Rhipe)
             rhinit()
          }
