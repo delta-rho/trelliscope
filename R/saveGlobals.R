38ca90399a1a53a7bb96551f76c9c32263f1fea5
Rounds, Jeremiah <jeremiah.rounds@pnnl.gov>
2015-11-19 15:52:02 -0800

    I believe the feature is now re-added to this branch and the vignette is right

--- R/saveGlobals.R
+++ R/saveGlobals.R
@@ -4,29 +4,6 @@
 # Time:  Fri Oct 23 15:33:03 2015
 #---------------------------------------------------------------------------------
 
-#' Save global environment data for displays
-#'
-#' BUG: This function doesn't work as intended because of scope for ...
-#' Makes data available in the global environment of all displays of a connection in trelliscope
-#'
-#' @param ... objects to be saved in globals.Rdata for trelliscope displays.
-#' @param conn vdb connection
-#' @details Useful to avoid bloating individual displays with global variables common to all displays in a data base.
-#' 
-#' @author Jeremiah Rounds
-#'
-#'
-#' @examples
-#' data(iris)
-#' saveGlobals(iris)
-saveGlobals = function(..., conn=getOption("vdbConn")){
-	validateVdbConn(conn)
-	dir = file.path(conn$path, "data")
-	if(!dir.exists(dir))
-		dir.create(dir)
-	save(..., file = file.path(dir, "globals.Rdata"))
-	
-}
 #' Path to global rdata in Trelliscope viewer.
 #' 
 #' \code{globalsFile} returns an appropriate file name to save to.
@@ -50,18 +27,7 @@ globalsFile = function(conn=getOption("vdbConn")){
 	
 }
 
-#' Copy R Files From Package
-#'
-#' Just a helper function that wraps copyRSource
-#' 
-#' @param from_package 
-#'
-#' @param conn vdbConn
-#'
-#' @export
-copyRFromPackage = function(from_package, conn=getOption("vdbConn")){
-	copyRSource(system.file("R", from_package), conn)
-}
+
 #' Copy files ending in .R from source directory
 #'
 #' These R files will be sourced in the global environment of the trelliscope viewer.
@@ -83,9 +49,9 @@ copyRSource = function(from_dir, conn=getOption("vdbConn")){
 		dir.create(dir, recursive=TRUE)
 	flist <- list.files(from_dir, "*.R$", full.names = TRUE)
 	message("Copying...")
-	print(flist)
-	file.copy(flist, dir)
-
+	#print(flist)
+	ret = file.copy(flist, dir)
+	ret
 }
 
 
